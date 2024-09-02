5b68403baf1e346c2348e8405ab87630cac239be
Bryan Peralta <bryan_tzunux@hotmail.com>
2024-09-02 17:16:51 -0600

    Eliminación de import innecesario

--- app/src/main/java/com/bpdevop/mediccontrol/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/bpdevop/mediccontrol/ui/screens/LoginScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
